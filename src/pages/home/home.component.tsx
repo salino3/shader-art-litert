e5e03b2616fa6456e950aa48c7d7734629a2e7e9
Salino3 <flaviobcn012@gmail.com>
2025-12-07 11:14:47 +0200

    feat: Add new Home page component displaying ShaderCanvas and ReactionDiffusionCanvas with associated styling.

--- src/pages/home/home.component.tsx
+++ src/pages/home/home.component.tsx
@@ -5,6 +5,7 @@ import "./home.styles.scss";
 export const Home: React.FC = () => {
   return (
     <div className="rootHome">
+      <h1 className="homeTitle">Shader Art LiteRT</h1>
       <details>
         <summary>WebGPU Wave Simulator</summary>
         <ShaderCanvas />
