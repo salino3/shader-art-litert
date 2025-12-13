7bee291a0ba2c78dce95b7f65ebcf4043327e61c
Salino3 <flaviobcn012@gmail.com>
2025-12-13 10:35:41 +0200

    save changes

--- generate-sw.js
+++ generate-sw.js
@@ -38,12 +38,14 @@ async function generateServiceWorker() {
   const urlsToCache = allFiles.map((file) => {
     // Remove the 'dist/' folder from the beginning of the path
     const relativePath = file.substring(distDir.length + 1);
-    return `${BASE_URL_PREFIX}${relativePath}`;
+    // Normalize path separators to forward slashes for URLs (Windows uses backslashes)
+    const normalizedPath = relativePath.replace(/\\/g, "/");
+    return `${BASE_URL_PREFIX}${normalizedPath}`;
   });
 
   // The base content of your Service Worker
   const swContent = `
-const CACHE_NAME = 'my-cache-v1';
+const CACHE_NAME = 'shader-art-cache-v1';
 const OFFLINE_URL = '${OFFLINE_PAGE}';
 
 // List of files to precache automatically
@@ -61,6 +63,26 @@ self.addEventListener('install', (event) => {
         return cache.addAll(urlsToCache);
       })
   );
+  // Force the waiting service worker to become the active service worker
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          // Delete old caches that don't match the current cache name
+          if (cacheName !== CACHE_NAME) {
+            console.log('Deleting old cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  // Take control of all pages immediately
+  return self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
@@ -70,17 +92,29 @@ self.addEventListener('fetch', (event) => {
     .then((response) => {
       // If the resource is in the cache, return it
       if (response) {
-          return response;
-        }
+        return response;
+      }
 
       // If not in cache, try to fetch it from the network
       return fetch(event.request)
+        .then((networkResponse) => {
+          // Cache successful network responses for future use
+          if (networkResponse && networkResponse.status === 200) {
+            const responseToCache = networkResponse.clone();
+            caches.open(CACHE_NAME).then((cache) => {
+              cache.put(event.request, responseToCache);
+            });
+          }
+          return networkResponse;
+        })
         .catch(() => {
           // If the network request fails (because we're offline),
           // and the request is for a navigation, serve the offline page.
           if (event.request.mode === 'navigate') {
             return caches.match(OFFLINE_URL);
           }
+          // For non-navigation requests, return undefined to let browser handle it
+          return undefined;
         });
     })
   );
